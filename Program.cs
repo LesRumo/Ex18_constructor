4be747d6e1aeff700f97e7324124baadc89bf9d1
SBICZA01\A253559 <Lesedi.Shirami@standardbank.co.za>
2023-12-05 14:16:17 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex18_constructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //an object called book1
+            Book book1 = new Book("Harry Potter", "JK Rowling", 400);
+
+            
+            Console.WriteLine(book1.title);
+            Console.ReadLine();
+        }
+    }
+}
